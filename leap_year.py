7661cd4fe71e5cc4539a5e1aa07745228d404516
Miklos Bowling <miklos.bowling@gmail.com>
2021-04-26 16:22:33 -0700

    Leap year code with error handling

--- leap_year.py
+++ leap_year.py
@@ -1,5 +1,11 @@
-print("Please enter a year:")
-year = int(input())
+year = None
+while year == None:
+    try:
+        print("Please enter a year:")
+        year = int(input())
+    except ValueError:
+        print("Input must be a positive integer\n")
+
 if year % 4 == 0:
     if year % 100 == 0:
         if year % 400 == 0:
